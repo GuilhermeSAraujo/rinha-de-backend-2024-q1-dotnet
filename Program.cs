ae5ca38efb56e3e5154749ef1bf349675bed58c4
Guilherme Souza Araújo <guilherme.souza@dtidigital.com.br>
2024-02-07 19:25:03 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/clientes/{id}/extrato", ([FromRoute] int id) =>
+{
+    return new { extrato = id };
+});
+
+app.Run();
